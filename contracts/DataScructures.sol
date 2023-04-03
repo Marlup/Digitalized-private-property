9383ce255a94be8cd0e638376999db92983bcdfa
Marlup <51297824+Marlup@users.noreply.github.com>
2023-04-03 20:37:49 +0200

    Create Data sctructures of contract
    First commit

---
+++ contracts/DataScructures.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
+ */
+contract DataStructures { 
+    enum VotingType {
+        StandBy,
+        All,
+        Majority
+    }
+}
